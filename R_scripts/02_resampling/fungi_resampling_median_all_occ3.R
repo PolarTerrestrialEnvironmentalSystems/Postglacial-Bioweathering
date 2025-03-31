566bd80dd1651a68f1c8e5552f8283cfd7b4952a
ucabuk <36818676+ucabuk@users.noreply.github.com>
2025-03-31 14:58:25 +0200

    Update fungi_resampling_median_all_occ3.R

--- R_scripts/02_resampling/fungi_resampling_median_all_occ3.R
+++ R_scripts/02_resampling/fungi_resampling_median_all_occ3.R
@@ -5,7 +5,7 @@ options(stringsAsFactors=FALSE)
 rm(list=ls())
 
 ### data table was exported from xlsx here csv2: sep=";", dec=","
-specseq=read.delim("fungi_assigned_resampling_occ3.csv", header = TRUE, sep = ";")
+specseq=read.delim("../01_filter_taxa/fungi_assigned_resampling_occ3.csv", header = TRUE, sep = ";")
 specseq <- specseq[,-1]
 
 ### check data is of required format, count data as integer/double, taxa names as strings
